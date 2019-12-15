bdda4298d283fdcba7e61b81839742d3c58f47df
jkalpeshgithub <kalpeshjadhav@gmail.com>
2019-12-15 16:55:16 +0530

    Update linux_vm.tf

--- linux_vm.tf
+++ linux_vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "linux_vm" {
-    ami                         = "ami-0cc293023f983ed53"
-    availability_zone           = "eu-central-1b"
+    ami                         = "ami-0d5d9d301c853a04a"
+    availability_zone           = "us-east-2b"
     ebs_optimized               = false
     instance_type               = "t2.micro"
     monitoring                  = false
